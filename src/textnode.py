6f86aaf63cc67c6d425a1c497707c57f061827f7
dragonbitestail <dragonbitestail@users.noreply.github.com>
2026-06-05 12:08:52 -0400

    Remove missed commented code

--- src/textnode.py
+++ src/textnode.py
@@ -45,14 +45,8 @@ def text_node_to_html_node(text_node: TextNode) -> LeafNode:
         case TextType.CODE:
             return LeafNode("code", text_node.text)
         case TextType.LINK:
-            #link_parts = text_node.text.split("](")
-            #value = link_parts[0][1:]
-            #url = link_parts[1][:-1]
             return LeafNode("a", text_node.text, props={"href": text_node.url})
         case TextType.IMAGE:
-            #link_parts = text_node.text.split("](")
-            #descr = link_parts[0][2:]
-            #url = link_parts[1][:-1]
             return LeafNode(tag="img", value="", props={"src": text_node.url, "alt": text_node.text})
 
         case _:
