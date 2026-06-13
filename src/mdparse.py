e88da5bfd43f5c238f63922b5f09aae1523d8dac
dragonbitestail <dragonbitestail@users.noreply.github.com>
2026-06-13 15:17:09 -0400

    This module broke me....
    Working on my own solution outside this project.
    If it works it will replace the Boots.Dev solution
    for split_nodes_image() and split_nodes_link()
    
    Disable debugging

--- src/mdparse.py
+++ src/mdparse.py
@@ -3,7 +3,10 @@ from textnode import *
 import logr
 import re
 
+import time
+
 #logr.DEBUG=True
+logr.DEBUG=False
 
 class BlockType(Enum):
     PARAGRAPH = "paragraph"
@@ -45,7 +48,7 @@ def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: 
 def extract_markdown_images(text: str) -> tuple[str, str]:
 
     matches = re.findall(r"!\[([^\]]+)\]\(([^\)]+)\)", text)
-    logr.log(f"extract_markdown_images: matches: {matches}")
+    logr.log(f"extract_markdown_images(): matches: {matches}")
 
     return matches
 
@@ -53,35 +56,68 @@ def extract_markdown_images(text: str) -> tuple[str, str]:
 def extract_markdown_links(text: str) -> tuple[str, str]:
 
     matches = re.findall(r"\[([^\]]+)\]\(([^\)]+)\)", text)
-    logr.log(f"extract_markdown_links: matches: {matches}")
+    logr.log(f"extract_markdown_links(): matches: {matches}")
 
     return matches
 
 
+# From Boot.Dev Solution: CH3, L3 Split Images and links
 def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        original_text = old_node.text
+        images = extract_markdown_images(original_text)
+        if len(images) == 0:
+            new_nodes.append(old_node)
+            continue
+        for image in images:
+            sections = original_text.split(f"![{image[0]}]({image[1]})", 1)
+            if len(sections) != 2:
+                raise ValueError("invalid markdown, image section not closed")
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+            new_nodes.append(
+                TextNode(
+                    image[0],
+                    TextType.IMAGE,
+                    image[1],
+                )
+            )
+            original_text = sections[1]
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TextType.TEXT))
+    return new_nodes
+
+def OLD_split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
     logr.log(f"split_nodes_image(): old_nodes: {old_nodes}")
 
     new_nodes = []
 
     for node in old_nodes:
-        logr.log(f"split_nodes_image(): old_node text: {node.text} of type {node.text_type}")
-        if node.text_type != TextType.TEXT:
+        logr.log(f"split_nodes_image(): old_node text: \"{node.text}\" of type \"{node.text_type}\"")
+        if node.text_type != TextType.TEXT or len(extract_markdown_images(node.text)) < 1:
             new_nodes.append(node)
             continue
 
         i = 0
         while i < len(node.text):
 
-            logr.log(f"split_nodes_image(): evaluating char {node.text[i]} <<<<<<<<<<<<<<<<<<<<<<")
+            logr.log(f"split_nodes_image(): OUTER while(): char \"{node.text[i]}\" <<<<<<<<<<<<<<<<<<<<<<")
             new_alt = ""
             new_url = ""
             new_text = ""
+
+            # We are at last char, this cannot be an image link
+            if i + 1 == len(node.text):
+                new_nodes.append(node)
+                break
             if node.text[i] == "!" and node.text[i+1] == "[":
-            #if node.text[i] == "[":
                 i += 2
-                #i += 1
                 logr.log(f"split_nodes_image(): accumulating alt text starting with char {node.text[i]} ![......................")
-                while node.text[i] != "]":
+                while i < len(node.text) and node.text[i] != "]":
                     new_alt += node.text[i]
                     i += 1
 
@@ -91,24 +127,58 @@ def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
                 else:
                     next
                 logr.log(f"split_nodes_image(): accumulating url text starting with char {node.text[i]} (.......................")
-                while node.text[i] != ")":
+                while i < len(node.text) and node.text[i] != ")":
                     new_url += node.text[i]
                     i += 1
                 new_nodes.append(TextNode(new_alt, TextType.IMAGE, url=new_url))
                 i += 1
+            elif i + 1 < len(node.text) and node.text[i + 1] != "[":
+                i += 1
             else:
                 logr.log(f"split_nodes_image(): accumulating plain text starting with char {node.text[i]} TTTTTTTTTTTTTTTTTTTTTTTTTTTTTT")
+                #while i < len(node.text) and node.text[i] != "!":
+                if i + 1 == len(node.text) and node.text[i] != "[" :
+                    logr.log(f"split_nodes_image(): BREAK on {node.text[i]}; end of string.")
+                    new_text += node.text[i]
+                    i += 1
+                    break
                 while i < len(node.text) and node.text[i] != "!":
                     new_text += node.text[i]
                     i += 1
-
                 new_nodes.append(TextNode(new_text, TextType.TEXT))
+
+            #time.sleep(2)
+            logr.log(f"split_nodes_image(): BOTTOM of OUTER while loop: new_nodes: {new_nodes}")
 
 
     logr.log(f"split_nodes_image(): return : {new_nodes} ***************************************")
     return new_nodes
 
+# From Boot.Dev Solution: CH3, L3 Split Images and links
 def split_nodes_link(old_nodes: list[TextNode]) -> list[TextNode]:
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        original_text = old_node.text
+        links = extract_markdown_links(original_text)
+        if len(links) == 0:
+            new_nodes.append(old_node)
+            continue
+        for link in links:
+            sections = original_text.split(f"[{link[0]}]({link[1]})", 1)
+            if len(sections) != 2:
+                raise ValueError("invalid markdown, link section not closed")
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+            new_nodes.append(TextNode(link[0], TextType.LINK, link[1]))
+            original_text = sections[1]
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TextType.TEXT))
+    return new_nodes
+
+def OLD_split_nodes_link(old_nodes: list[TextNode]) -> list[TextNode]:
     logr.log(f"split_node_link(): old_nodes: {old_nodes}")
 
     new_nodes = []
@@ -204,22 +274,22 @@ def block_to_block_type(block):
     r_ord_block_start = re.compile(r"^([\d]+)\. ")
 
     if r_heading.match(block):
-        logr.log(f"We have a heading block ####################")
+        logr.log(f"block_to_block_type(): We have a heading block ####################")
         block_type = BlockType.HEADING
     elif r_mlcode.match(block):
-        logr.log(f"We have a multiline code block ````````````````````")
+        logr.log(f"block_to_block_type(): We have a multiline code block ````````````````````")
         block_type = BlockType.CODE
     elif r_qblock_start.match(block):
-        logr.log(f"We have a **possible** quote block >>>>>>>>>>>>>>>>>>>>")
+        logr.log(f"block_to_block_type(): We have a **possible** quote block >>>>>>>>>>>>>>>>>>>>")
         block_type = BlockType.QUOTE
     elif r_unord_block_start.match(block):
-        logr.log(f"We have a **possible** unordered_list block - - - - - - - - - -")
+        logr.log(f"block_to_block_type(): We have a **possible** unordered_list block - - - - - - - - - -")
         block_type = BlockType.UNORDERED_LIST
     elif r_ord_block_start.match(block) and all_lines_ordered(r_ord_block_start, block):
-        logr.log(f"We have a **possible** ordered_list block N.N.N.N.N.N.N.N.N.N.")
+        logr.log(f"block_to_block_type(): We have a **possible** ordered_list block N.N.N.N.N.N.N.N.N.N.")
         block_type = BlockType.ORDERED_LIST
 
-    logr.log(f"block_to_block_type: OUT: block_type: {block_type}")
+    logr.log(f"block_to_block_type(): OUT: block_type: {block_type}")
     return block_type
 
 
