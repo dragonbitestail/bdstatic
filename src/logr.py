aa740d836eb316e5a62affc8b97083e2db743001
dragonbitestail <dragonbitestail@users.noreply.github.com>
2026-06-13 14:19:29 -0400

    Commented out code to output module in output
    This was a solution found on SO, and it introduced a subtle bug
    in certain circumstances so disabling for now.
    
    At some point should just refactor logging to use Python's standard
    logging module which has this feature amongst others.

--- src/logr.py
+++ src/logr.py
@@ -1,9 +1,10 @@
+import inspect
+
 
 DEBUG=False
 
 def log(msg):
-
+    #parent_frame_info = inspect.stack()[1]
+    #mod = inspect.getmodule(parent_frame_info.frame)
+    #DEBUG and print(f"[DEBUG-{mod.__name__}] {msg}")
     DEBUG and print(f"[DEBUG] {msg}")
-
-    #print(f"[TEST] {msg}")
-
